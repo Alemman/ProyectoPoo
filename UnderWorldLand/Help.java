52ec250d0ba9c348d9f6131b46568743ac593192
Alemman <gstavo.aleman@gmail.com>
2020-12-11 14:29:46 -0600

    #5 - clase-ayuda

--- UnderWorldLand/Help.java
+++ UnderWorldLand/Help.java
@@ -1,14 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
+
 public class Help extends Window
-{
+{  
     public Help()
     {    
        prepareWindow();       
     }
     
     public void prepareWindow(){
-        showText("Help.txt", 250, 100);
-        addObject(new BtnMainMenu(),X_BUTTON_MAIN,Y_BUTTON_MAIN);
-    }
+       GreenfootImage fondoHelp = new GreenfootImage("images/como-jugar.png");
+       fondoHelp.scale(WIDTH_WORLD,HEIGHT_WORLD);
+       setBackground(fondoHelp);
+       addObject(new BtnMainMenu(),X_BUTTON_MAIN,Y_BUTTON_MAIN);
+    }   
 }
--- UnderWorldLand/Menu.java
+++ UnderWorldLand/Menu.java
@@ -3,10 +3,9 @@ import greenfoot.*;
 
 public class Menu extends Window
 {
-    private static final GreenfootImage fondoMenu = new GreenfootImage("images/main-menu.jpg");
-
     public Menu()
     {
+       GreenfootImage fondoMenu = new GreenfootImage("images/main-menu-.jpg");
        fondoMenu.scale(WIDTH_WORLD,HEIGHT_WORLD);
        setBackground(fondoMenu);
         
@@ -20,11 +19,5 @@ public class Menu extends Window
         addObject(new BtnRecord(),BUTTON_X,BUTTON_Y + BUTTON_GAP);       
         addObject(new BtnHelp(),BUTTON_X,BUTTON_Y + BUTTON_GAP*2);        
         addObject(new BtnExit(),BUTTON_X,BUTTON_Y + BUTTON_GAP*3);
-        
-        showText("UnderWorld Land", 250, 100);
-        
     }
-    
-    
-   
 }
